304792231382dec65ffcde44e6b842abfb641a32
Kionling <djvdjv209@gmail.com>
2020-09-29 11:30:21 -0700

    Added server functionality to respond to route changes made by client

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express();
 const PORT = process.env.PORT || 4444
+const routes = require("./routes/html-routes")
 
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
@@ -9,5 +10,5 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
   }
 
-  
+  app.use(routes)
   app.listen(PORT, () => console.log("Server listening on port: " + PORT))
